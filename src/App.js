4e06a510ffe4c7b0073eac1b0891b5d080812e6e
Seesti <113047119+Seesti@users.noreply.github.com>
2022-11-04 17:25:07 +0200

    repo created

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './index.css';
+import Nav from './components/Nav';
+import Landing from './components/Landing';
+import Highlights from './components/ui/Highlight';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Landing />
+      <Highlights />
+    </div>
+  );
+}
+
+export default App;
