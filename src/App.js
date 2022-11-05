54f7cb70bad6a134e098f73b20cf1b8d93e844bf
Seesti <113047119+Seesti@users.noreply.github.com>
2022-11-05 19:41:46 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './pages/Home';
 import Books from './pages/Books';
 import { books } from './data';
+import BookInfo from './pages/BookInfo';
 
 function App() {
   return (
@@ -13,8 +14,8 @@ function App() {
     <div className="App">
       <Nav />
       <Route path="/" exact component={Home} />
-      <Route path="/books" render={() => <Books books={books} />} />
-      <Home />
+      <Route path="/books" exact render={() => <Books books={books} />} />
+      <Route path='/books/:id' exact render={() => <BookInfo books={books} />} />
       <Footer />
     </div>
     </Router>
